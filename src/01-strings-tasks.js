06df8d29cd850bf4a442e1f1fe21b8eb9a9965cb
Sasha Vrublevskaya <Belarus>
2020-12-03 00:13:38 +0300

    .

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -80,8 +80,8 @@ function extractNameFromTemplate(value) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(value) {
-  return value.charAt(0);
+function getFirstChar(/*value*/) {
+  throw new Error('Not implemented');
 }
 
 /**
